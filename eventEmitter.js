280f0c38f6fc9ee8dfc0528c3ab3cec93bcf82ec
JackYuan19 <2821946800@qq.com>
2022-12-09 16:09:22 +0800

    EventEmitter

--- eventEmitter.js
+++ eventEmitter.js
@@ -27,7 +27,7 @@
             this.on = function (eventName, listener) {
                 if (!eventName)
                     return;
-                var events = this.events;
+                var events = this.__events;
                 var listeners = events[eventName] = events[eventName] || [];
                 var listenerIsWrapped = typeof listener === "object";
                 // 不重复添加事件
@@ -51,8 +51,77 @@
                     once: true
                 });
             };
+            /**
+             * 删除事件
+             * @param {string} eventName 事件名称
+             * @param {Function} listener 监听器函数
+             * @return {Object} 可链式调用
+            */
+            this.off = function (eventName, listener) {
+                var listeners = this.__events[eventName];
+                if (!listeners)
+                    return;
+                var index = NaN;
+                for (var i = 0, len = listeners.length; i < len; i++) {
+                    if (listeners[i] && listeners[i].listener === listener) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (!isNaN(index)) {
+                    listeners.splice(index, 1, null);
+                }
+                return this;
+            };
+            /**
+             * 删除某一个类型所有事件或者所有事件
+             * @param {string} eventName 事件名称
+            */
+            this.allOff = function (eventName) {
+                if (eventName && this.events[eventName]) {
+                    this.__events[eventName] = [];
+                }
+                else {
+                    this.events = {};
+                }
+            };
+            /**
+             * 触发事件
+             * @param {string} eventName 事件名称
+             * @param {Array} args 传入监听器函数的参数，使用数组形式传入
+             * @return {Object} 可调式调用
+            */
+            this.emit = function (eventName, args) {
+                var listeners = this.__events[eventName];
+                if (!listeners)
+                    return;
+                for (var i = 0, len = listeners.length; i < len; i++) {
+                    var listener = listeners[i];
+                    if (listener) {
+                        listener.listener.apply(this, args || []);
+                        if (listener.once) {
+                            this.off(eventName, listener.listener);
+                        }
+                    }
+                }
+                return this;
+            };
         }
         EventEmitter.VERSION = "1.0.0";
         return EventEmitter;
     }());
+
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = EventEmitter;
+        }
+
+        exports.EventEmitter = EventEmitter;
+
+    } 
+    else {
+        root.EventEmitter = EventEmitter;
+    }
+
 }());
